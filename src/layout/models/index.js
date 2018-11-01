3f35653026dc6ee35c63af93c3ddbaa1fffad868
zuohw1 <zuohongwei1986@163.com>
2018-11-01 19:50:45 +0800

    人事管理

--- src/layout/models/index.js
+++ src/layout/models/index.js
@@ -6,53 +6,6 @@ export default {
     collapsed: false,
     menus: [
       {
-        id: 1,
-        menuName: '组织管理',
-        pid: 0,
-        iconUrl: 'sync',
-      },
-      {
-        id: 101,
-        menuName: '组织信息查询',
-        url: '/org/search',
-        pid: 1,
-        iconUrl: 'sync',
-      },
-      {
-        id: 102,
-        menuName: '组织批文前置流程',
-        url: '/org/approval',
-        pid: 1,
-        iconUrl: 'sync',
-      },
-      {
-        id: 2,
-        menuName: '职位管理',
-        pid: 0,
-        iconUrl: 'user',
-      },
-      {
-        id: 201,
-        menuName: '关键职责库维护',
-        url: '/post/keyresp',
-        pid: 2,
-        iconUrl: 'user',
-      },
-      {
-        id: 202,
-        menuName: '关键职责库查询',
-        url: '/post/keyrespquery',
-        pid: 2,
-        iconUrl: 'user',
-      },
-      {
-        id: 203,
-        menuName: '关键职责职级列表',
-        url: '/post/keyresprank',
-        pid: 2,
-        iconUrl: 'user',
-      },
-      {
         id: 3,
         menuName: '人员管理',
         url: null,
