1eb08f19d10525087d297f42af9b421d63c6e69f
zuohw1 <zuohongwei1986@163.com>
2018-11-06 09:16:00 +0800

    人事管理

--- src/layout/containers/index.js
+++ src/layout/containers/index.js
@@ -2,14 +2,15 @@ import { connect } from 'dva';
 
 import Layout from '../components/index';
 
+/* 建立组件跟 store 的 state 的映射关系 */
 const mapStateToProps = (state) => {
   return {
     ...state.layout,
   };
 };
 
+/* 直接将action包装成可以被调用的函数 */
 const mapDispatchToProps = dispatch => ({
-  // actions: bindActionCreators(actions, dispatch),
   dispatch,
 });
 
